bc9081ab0bdd68fe173362e31d549c20b249b32d
Hung Nguyen <nthungdev@gmail.com>
2019-03-15 00:43:55 -0400

    update login layout

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:deliverit/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(DeliverIt());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
