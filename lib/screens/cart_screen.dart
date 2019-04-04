1d35d6147e7cc63137a23ba7bbc9b4c078a0ec87
Keith Prevost <rickyprev@yahoo.com>
2019-04-04 16:49:55 -0400

    need to seperate widgets into files and sanitize user input

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,5 +1,5 @@
-import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
+import 'package:money/money.dart';
 
 class CartScreen extends StatelessWidget {
   CartScreen({Key key}) : super(key: key);
@@ -18,10 +18,45 @@ class CartScreen extends StatelessWidget {
     var numItemsController = TextEditingController();
     var itemPrice = 10.20;
     var totalPrice = 0.0;
-    var taxes = 0;
-    var deliverFee = 0.0;
+    var taxes = 1.8;
+    var deliverFee = 7.5;
 
     var itemData = [
+      {
+        "itemName": "taco",
+        "itemPrice": 10.20,
+        "numItemsController": TextEditingController(),
+        "photoLink":
+            "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
+      },
+      {
+        "itemName": "taco",
+        "itemPrice": 10.20,
+        "numItemsController": TextEditingController(),
+        "photoLink":
+            "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
+      },
+      {
+        "itemName": "taco",
+        "itemPrice": 10.20,
+        "numItemsController": TextEditingController(),
+        "photoLink":
+            "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
+      },
+      {
+        "itemName": "taco",
+        "itemPrice": 10.20,
+        "numItemsController": TextEditingController(),
+        "photoLink":
+            "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
+      },
+      {
+        "itemName": "taco",
+        "itemPrice": 10.20,
+        "numItemsController": TextEditingController(),
+        "photoLink":
+            "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
+      },
       {
         "itemName": "taco",
         "itemPrice": 10.20,
@@ -51,133 +86,127 @@ class CartScreen extends StatelessWidget {
       totalPrice += item["itemPrice"];
     });
 
-    // List<Widget> itemList = [
-    //   CartItem(
-    //     itemName: "taco",
-    //     itemPrice: 10.20,
-    //     numItemsController: TextEditingController(),
-    //     photoLink:
-    //         "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
-    //   ),
-    //   CartItem(
-    //     itemName: "taco",
-    //     itemPrice: 10.20,
-    //     numItemsController: TextEditingController(),
-    //     photoLink:
-    //         "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
-    //   ),
-    //   CartItem(
-    //     itemName: "taco",
-    //     itemPrice: 10.20,
-    //     numItemsController: TextEditingController(),
-    //     photoLink:
-    //         "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
-    //   ),
-    //   CartItem(
-    //     itemName: "taco",
-    //     itemPrice: 10.20,
-    //     numItemsController: TextEditingController(),
-    //     photoLink:
-    //         "https://media.wired.com/photos/592713647034dc5f91bed905/master/w_582,c_limit/STORY-GettyImages-498648767a.jpg",
-    //   )
-    // ];
-
     return Scaffold(
+        appBar: AppBar(
+          title: Text("Your Order"),
+          actions: <Widget>[buildButton()],
+        ),
         body: SafeArea(
-      child: Container(
-          child: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(
-              top: 15,
-              left: 20,
-            ),
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Your Order",
-                textScaleFactor: 2,
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-          Column(children: <Widget>[
-            ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(20.0),
-              children: itemList,
-            )
-          ]),
-          Column(
+          child: Container(
+              child: Column(
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text(
-                    totalPrice.toString(),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 75.0, right: 10),
-                    child: Text("US AMOUNT"),
-                  ),
-                ],
+              // buildHeader(),
+              Expanded(
+                child: CartDetails(
+                    itemList: itemList,
+                    totalPrice: totalPrice,
+                    taxes: taxes,
+                    deliverFee: deliverFee),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text("TAXES"),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 75.0, right: 10),
-                    child: Text("US AMOUNT"),
-                  ),
-                ],
+            ],
+          )),
+        ));
+  }
+
+  Widget buildButton() {
+    return new FlatButton(
+        onPressed: () {},
+        padding: const EdgeInsets.only(left: 8.0),
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: new Text(
+          "Checkout",
+        ));
+  }
+
+  Container buildHeader() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 15,
+        left: 20,
+      ),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Text(
+          "Your Order",
+          textScaleFactor: 2,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class CartDetails extends StatelessWidget {
+  const CartDetails({
+    Key key,
+    @required this.itemList,
+    @required this.totalPrice,
+    @required this.taxes,
+    @required this.deliverFee,
+  }) : super(key: key);
+
+  final List<Widget> itemList;
+  final double totalPrice;
+  final double taxes;
+  final double deliverFee;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: <Widget>[
+      Column(
+        // shrinkWrap: true,
+        // padding: const EdgeInsets.all(20.0),
+        children: itemList,
+      ),
+      Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                "SUBTOTAL",
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text("DELIVERY FEE"),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 75.0, right: 10),
-                    child: Text("US AMOUNT"),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text("TOTAL"),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 75.0, right: 10),
-                    child: Text("US AMOUNT"),
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.only(left: 70.0, top: 50),
-                    child: new RaisedButton(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        textColor: Colors.black,
-                        color: Colors.blue,
-                        child: new Text("Back")),
-                  ),
-                  Container(
-                      margin: const EdgeInsets.only(left: 70.0, top: 50),
-                      child: new RaisedButton(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          textColor: Colors.white,
-                          color: Colors.blue,
-                          child: new Text("Checkout")))
-                ],
+              Padding(
+                padding: const EdgeInsets.only(left: 75.0, right: 10),
+                child: Text("\$" + (totalPrice.toStringAsFixed(2))),
               ),
             ],
           ),
-          // ],
-          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text("TAXES"),
+              Padding(
+                padding: const EdgeInsets.only(left: 75.0, right: 10),
+                child: Text("\$" + taxes.toStringAsFixed(2)),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text("DELIVERY FEE"),
+              Padding(
+                padding: const EdgeInsets.only(left: 75.0, right: 10),
+                child: Text("\$" + deliverFee.toStringAsFixed(2)),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text("TOTAL"),
+              Padding(
+                padding: const EdgeInsets.only(left: 75.0, right: 10),
+                child: Text("\$" +
+                    (deliverFee + taxes + totalPrice).toStringAsFixed(2)),
+              ),
+            ],
+          ),
         ],
-      )),
-    ));
+      ),
+    ]);
   }
 }
 
@@ -216,6 +245,7 @@ class CartItem extends StatelessWidget {
                     height: 50,
                     width: 50,
                     child: TextField(
+                      keyboardType: TextInputType.number,
                       controller: numItemsController,
                     )))),
         Expanded(
